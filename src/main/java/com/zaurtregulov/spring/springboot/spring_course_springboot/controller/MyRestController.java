c2fcf2bb243d9eae98790f0c9f4390e239226907
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-08 13:09:55 +0300

    99 5.06

--- src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/controller/MyRestController.java
@@ -22,33 +22,33 @@ public class MyRestController {
         return allEmployees;
     }
 
-//    @GetMapping("/employees/{id}")
-//    public Employee getEmployee(@PathVariable int id) {
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        return employee;
-//    }
-//
-//    @PostMapping("/employees")
-//    public Employee addNewEmployee(@RequestBody Employee employee) {
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @PutMapping("/employees")
-//    public Employee updateEmployee(@RequestBody Employee employee) {
-//        employeeService.saveEmployee(employee);
-//
-//        return employee;
-//    }
-//
-//    @DeleteMapping("employees/{id}")
-//    public String deleteEmployee(@PathVariable int id) {
-//        Employee employee = employeeService.getEmployee(id);
-//
-//        employeeService.deleteEmployee(id);
-//
-//        return "Employee with id = " + id + " was deleted from database";
-//    }
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with id = " + id + " was deleted from database";
+    }
 }
--- src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/dao/EmployeeDAOImp.java
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/dao/EmployeeDAOImp.java
@@ -1,12 +1,12 @@
 package com.zaurtregulov.spring.springboot.spring_course_springboot.dao;
 
 import com.zaurtregulov.spring.springboot.spring_course_springboot.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+//import org.hibernate.Session;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -18,29 +18,52 @@ public class EmployeeDAOImp implements EmployeeDAO {
     @Override
     public List<Employee> getAllEmployees() {
 
-        Session session = entityManager.unwrap(Session.class);
-        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+////////////////////////////////Реализация через Hibernate//////////////////////
+//        Session session = entityManager.unwrap(Session.class);
+//        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class).getResultList();
+////////////////////////////////или///////////////////
+//        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+//        List<Employee> allEmployees = query.getResultList();
+
+////////////////////////////////Реализация через JPA//////////////////////////
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> allEmployees = query.getResultList();
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
-//        Session session = sessionFactory.getCurrentSession();
+    @Override
+    public void saveEmployee(Employee employee) {
+////////////////////////////////Реализация через Hibernate//////////////////////
+//        Session session = entityManager.unwrap(Session.class);
 //        session.saveOrUpdate(employee);
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
+
+////////////////////////////////Реализация через JPA////////////////////////////
+        entityManager.merge(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+////////////////////////////////Реализация через Hibernate//////////////////////
+//        Session session = entityManager.unwrap(Session.class);
 //        Employee employee = session.get(Employee.class, id);
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
-//        Session session = sessionFactory.getCurrentSession();
+
+////////////////////////////////Реализация через JPA////////////////////////////
+        Employee employee = entityManager.find(Employee.class, id);
+
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+////////////////////////////////Реализация через Hibernate//////////////////////
+//        Session session = entityManager.unwrap(Session.class);
 //        Query<Employee> query = session.createQuery("delete from Employee where id =: employeeId");
 //        query.setParameter("employeeId", id);
 //        query.executeUpdate();
-//    }
+
+////////////////////////////////Реализация через JPA///////////////////////////
+        Query query = entityManager.createQuery("from Employee where id =: employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
